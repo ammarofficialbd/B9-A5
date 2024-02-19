5899c632de73b78799c45273207da250f285dbb3
Ammar Junior <59601243+ammarofficialbd@users.noreply.github.com>
2024-02-18 22:13:33 -0800

    All functionalities Are created and Updated

--- index.js
+++ index.js
@@ -4,20 +4,38 @@ function Menu(e) {
   e.name === 'menu' ? (e.name = "close", list.classList.add('top-[70px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[70px]'), list.classList.remove('opacity-100'))
 }
 
+
 /* Seat Booking Section */
 
-//Select Seat 
+//Select All Variables
 
 const seatAll = document.querySelectorAll('.seat')
+let totalSeats = document.getElementById('total-seat')
+let seatCount = document.getElementById('seat__number')
+let grandTotalElement = document.getElementById('grand__total')
+const discountPrice = document.getElementById('discountPrice')
+const couponInput = document.getElementById('coupon');
+const PassengerName = document.getElementById('passengerName');
+const phoneNumberInput = document.getElementById('phoneNumber');
+const applyButton = document.getElementById('applyButton');
+const submitButton = document.getElementById('submitButton');
+const inputField = document.getElementById('input_field')
+
+//console.log(totalSeats)
+let availableSeats = 40 ; 
 //console.log(seat)
 const seatPrice = 550;
 let selectedSeats = []
+const maxSelectedSeats = 4
 
+
+
+/* Logic Section */
 for (let i = 0; i < seatAll.length; i++) {
   const seat = seatAll[i];
   seat.addEventListener("click", () => {
     const seatId = seat.id;
-    console.log(seatId)
+   // console.log(seatId)
     const seatName = seat.innerText
     const seatInfo = document.getElementById("seat-info")
     seat.classList.add("selected")
@@ -27,7 +45,7 @@ for (let i = 0; i < seatAll.length; i++) {
       if(!selectedSeats.includes(seatId)){
         const newRow = document.createElement("tr");
         const cellA1 = createTableCell(seatName);
-        console.log(cellA1)
+       // console.log(cellA1)
   
         const cellEconomy = createTableCell("Economy");
         const cellPrice = createTableCell("550");
@@ -46,28 +64,97 @@ for (let i = 0; i < seatAll.length; i++) {
           cell.textContent = content;
           return cell;
         }
-        seat.classList.add('bg-green-300');
+        seat.classList.add('bg-green-400');
         selectedSeats.push(seatId);
-        console.log(selectedSeats)
+        availableSeats--;
+        updateAvailableSeats()
+        updateSeatsCount()
+        updateTotalPrice()
       }else{
         alert("You cant select once seat More time")
+
       }
       
     } else{
-      seat.classList.remove('bg-green-300');
+      seat.classList.remove('bg-green-400');
       alert("Can't Selected more then 4 seats and can't")
     }
 
-  updateSelectedSeats();
+
+  })
 
 
  
-  })
 /* update Total price */
-  function updateSelectedSeats() {
+  function updateTotalPrice() {
     const totalPriceElement = document.getElementById('totalPrice');
     const totalPrice = selectedSeats.length * seatPrice;
+    const discount = applyDiscount() ? getDiscount(totalPrice) : 0;
+    const grandTotal = totalPrice - discount;
     totalPriceElement.textContent = `BDT: ${totalPrice} TK`;
-    console.log(totalPrice)
+    //console.log(totalPrice)
+    return {grandTotal, discount}
+  }
+  function updateAvailableSeats() {
+    console.log(availableSeats)
+    totalSeats.innerText = `${availableSeats}`;
+  }
+  function updateSeatsCount() {
+    console.log(selectedSeats.length)
+    seatCount.innerText = `${selectedSeats.length}`;
   }
 }
+  
+
+function applyDiscount() {
+  const couponValue = couponInput.value.trim().toLowerCase();
+  return couponValue === 'new 15' || couponValue === 'couple 20';
+}
+
+function getDiscount(totalPrice) {
+  const couponValue = couponInput.value.trim().toLowerCase();
+  return couponValue === 'new 15' ? (totalPrice * 15) / 100 : (totalPrice * 20) / 100;
+}
+
+
+/*  Handle coupon Apply Button */
+function handleApplyButtonClick(e) {
+    e.preventDefault()
+    const {discount, grandTotal} = updateTotalPrice()
+    if (selectedSeats.length < maxSelectedSeats) {
+      alert(`Minimum ${maxSelectedSeats} seats must be selected before applying the coupon.`);
+    }
+    else if (!applyDiscount()) {
+      alert('Invalid coupon. Please enter a valid coupon code.');
+    } else {
+      updateTotalPrice();
+
+      grandTotalElement.textContent = `BDT: ${grandTotal} TK`;
+      discountPrice.textContent = `
+      Discount price : BDT - ${discount} TK`;  
+      inputField.classList.add('hidden')
+    }
+    
+}
+
+function handleSubmitButtonClick(e) {
+  e.preventDefault()
+  if(PassengerName.value && phoneNumberInput.value && selectedSeats.length > 0){
+    window.location.href = 'succeed.html';
+  }else{
+    alert("Plz Provide required Field")
+  }
+ 
+}
+
+
+/* Event Listener */
+
+  applyButton.addEventListener('click', function (e) {
+    handleApplyButtonClick(e);
+  });
+
+  submitButton.addEventListener('click', function (e) {
+    handleSubmitButtonClick(e);
+  });
+
